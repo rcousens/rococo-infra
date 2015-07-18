d52825c0373c12c731cd414784246083b12933e0
Ross Cousens <ross@hatesmail.com>
2015-07-18 15:29:07 +1000

    Restructure directories

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+DEBIAN_FRONTEND=noninteractive
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get -y install ansible
---
+++ setup.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ansible-galaxy install -p .ansible/roles -r requirements.yml
